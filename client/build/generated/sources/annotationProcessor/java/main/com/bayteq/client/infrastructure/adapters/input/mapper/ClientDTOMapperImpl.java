1e2f08bab4d2faedfe73a75614f51c7016e730d7
Anderson Cristóbal Zambrano Balseca <anderson.zambrano@sofka.local>
2025-03-19 12:41:08 -0500

    build

---
+++ client/build/generated/sources/annotationProcessor/java/main/com/bayteq/client/infrastructure/adapters/input/mapper/ClientDTOMapperImpl.java
@@ -0,0 +1,56 @@
+package com.bayteq.client.infrastructure.adapters.input.mapper;
+
+import com.bayteq.client.domain.models.Client;
+import com.bayteq.client.infrastructure.adapters.input.dto.ClientRequestDTO;
+import com.bayteq.client.infrastructure.adapters.input.dto.ClientResponseDTO;
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2025-03-19T12:05:45-0500",
+    comments = "version: 1.5.5.Final, compiler: IncrementalProcessingEnvironment from gradle-language-java-8.13.jar, environment: Java 17.0.12 (Oracle Corporation)"
+)
+@Component
+public class ClientDTOMapperImpl implements ClientDTOMapper {
+
+    @Override
+    public Client toClient(ClientRequestDTO dto) {
+        if ( dto == null ) {
+            return null;
+        }
+
+        Client client = new Client();
+
+        client.setName( dto.getName() );
+        client.setLastName( dto.getLastName() );
+        client.setDocumentNumber( dto.getDocumentNumber() );
+        client.setEmail( dto.getEmail() );
+        client.setPhoneNumber( dto.getPhoneNumber() );
+        client.setBirthDate( dto.getBirthDate() );
+        client.setAddress( dto.getAddress() );
+
+        return client;
+    }
+
+    @Override
+    public ClientResponseDTO toResponseDTO(Client client) {
+        if ( client == null ) {
+            return null;
+        }
+
+        ClientResponseDTO clientResponseDTO = new ClientResponseDTO();
+
+        clientResponseDTO.setId( client.getId() );
+        clientResponseDTO.setName( client.getName() );
+        clientResponseDTO.setLastName( client.getLastName() );
+        clientResponseDTO.setDocumentNumber( client.getDocumentNumber() );
+        clientResponseDTO.setEmail( client.getEmail() );
+        clientResponseDTO.setPhoneNumber( client.getPhoneNumber() );
+        clientResponseDTO.setBirthDate( client.getBirthDate() );
+        clientResponseDTO.setAddress( client.getAddress() );
+        clientResponseDTO.setActive( client.isActive() );
+
+        return clientResponseDTO;
+    }
+}
---
+++ client/build/generated/sources/annotationProcessor/java/main/com/bayteq/client/infrastructure/adapters/output/mapper/ClientEntityMapperImpl.java
@@ -0,0 +1,57 @@
+package com.bayteq.client.infrastructure.adapters.output.mapper;
+
+import com.bayteq.client.domain.models.Client;
+import com.bayteq.client.infrastructure.adapters.output.entities.ClientEntity;
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2025-03-19T12:05:45-0500",
+    comments = "version: 1.5.5.Final, compiler: IncrementalProcessingEnvironment from gradle-language-java-8.13.jar, environment: Java 17.0.12 (Oracle Corporation)"
+)
+@Component
+public class ClientEntityMapperImpl implements ClientEntityMapper {
+
+    @Override
+    public ClientEntity toEntity(Client client) {
+        if ( client == null ) {
+            return null;
+        }
+
+        ClientEntity clientEntity = new ClientEntity();
+
+        clientEntity.setId( client.getId() );
+        clientEntity.setName( client.getName() );
+        clientEntity.setLastName( client.getLastName() );
+        clientEntity.setDocumentNumber( client.getDocumentNumber() );
+        clientEntity.setEmail( client.getEmail() );
+        clientEntity.setPhoneNumber( client.getPhoneNumber() );
+        clientEntity.setBirthDate( client.getBirthDate() );
+        clientEntity.setAddress( client.getAddress() );
+        clientEntity.setActive( client.isActive() );
+
+        return clientEntity;
+    }
+
+    @Override
+    public Client toDomain(ClientEntity entity) {
+        if ( entity == null ) {
+            return null;
+        }
+
+        Client client = new Client();
+
+        client.setId( entity.getId() );
+        client.setName( entity.getName() );
+        client.setLastName( entity.getLastName() );
+        client.setDocumentNumber( entity.getDocumentNumber() );
+        client.setEmail( entity.getEmail() );
+        client.setPhoneNumber( entity.getPhoneNumber() );
+        client.setBirthDate( entity.getBirthDate() );
+        client.setAddress( entity.getAddress() );
+        client.setActive( entity.isActive() );
+
+        return client;
+    }
+}
